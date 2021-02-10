25c421df3a77c04d1fb08fd6a5a99fae4232a739
Cem GEÇGEL <aa-albara@hotmail.com>
2021-02-10 17:47:41 +0300

    3.15
    We have gone old school

--- test_datalogger.c
+++ test_datalogger.c
@@ -1,7 +1,7 @@
 /*
  * ME331 FALL2020 Term Project Group 7
  * Author: Cem
- * Version: 3.14
+ * Version: 3.15
  *
  * This version test the SD card and the temperature sensor.
  *
@@ -27,26 +27,11 @@
 #define PIN_GREEN 9
 
 void setup() {
-	pinMode(PIN_RED, OUTPUT);
-	pinMode(PIN_GREEN, OUTPUT);
 	pinMode(PIN_SD_CS, OUTPUT);
 	digitalWrite(PIN_SD_CS, HIGH);
-	// Set up the SD card.
-	// Initialize the SD library.
-	if (SD.begin(PIN_SD_CS)) {
-		// Open the output file.
-		File currentFile = SD.open("output.bin", FILE_WRITE);
-		// If the file could be opened.
-		if (currentFile) {
-		}
-		else {
-			digitalWrite(PIN_GREEN, HIGH);
-			// Set the state to DONE.
-		}
-	}
-	else {
-		digitalWrite(PIN_RED, HIGH);
-	}
+	SD.begin(PIN_SD_CS);
+	File currentFile = SD.open("test.txt", FILE_WRITE);
+	currentFile.write("Hello World");
 }
 
 void loop() {
