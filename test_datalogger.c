6fd038cc1f042734695b8846bea972fe19e96bac
Cem GEÇGEL <aa-albara@hotmail.com>
2021-02-10 17:50:27 +0300

    3.15
    Cleaned up

--- test_datalogger.c
+++ test_datalogger.c
@@ -8,23 +8,12 @@
  * Created on 28.1.2021, 21:44
  */
 
- // L I B R A R I E S
- // ~~~~~~~~~~~~~~~~~
-
  // For SD Card
 #include <SPI.h>
 #include <SD.h>
 
-// C O N S T A N T S
-// ~~~~~~~~~~~~~~~~~
-
-// Temperature Sensor
-#define PIN_TEMPERATURE_SENSOR A0
 // SD Card Chip Select
 #define PIN_SD_CS 10
-// Debug LEDS
-#define PIN_RED 8
-#define PIN_GREEN 9
 
 void setup() {
 	pinMode(PIN_SD_CS, OUTPUT);
